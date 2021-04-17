ae5799d282cf7c22f0f2b03fd6db86b970c9d6c6
Andrey Kuksa <andrey20011005@gmail.com>
2021-04-16 17:04:12 -0700

    Task 2 completed

---
+++ lab7_2/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  lab7_2
+//
+//  Created by Andrey Kuksa on 4/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundLabel: UILabel!
+    @IBOutlet weak var backgroundSwitch: UISwitch!
+    
+    let firstBackground = "Backgrond image : bg1.jpg"
+    let secondBackground = "Backgrond image : bg2.jpg"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        backgroundLabel.text = firstBackground
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "bg1")!)
+        
+    }
+    
+    @IBAction func switchTapped(_ sender: Any) {
+        if backgroundSwitch.isOn {
+            backgroundLabel.text = firstBackground
+            view.backgroundColor = UIColor(patternImage: UIImage(named: "bg2")!)
+        } else {
+            backgroundLabel.text = secondBackground
+            view.backgroundColor = UIColor(patternImage: UIImage(named: "bg1")!)
+        }
+        
+    }
+}
+
